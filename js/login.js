f5a03e0d3d1c1ced29be3e3bf6a321b1d5c20c2c
ovsbrian <ovsbrian@gmail.com>
2022-11-11 21:40:23 -0300

    desafio 1

--- js/login.js
+++ js/login.js
@@ -18,15 +18,17 @@ ver.addEventListener("click", function(){
     }
 })
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
-
+function jwtDecode (res){
+ console.log (decode(res.credential))
+}
+function decode(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+};
 
 
 // Función para validar la entrada del usuario
